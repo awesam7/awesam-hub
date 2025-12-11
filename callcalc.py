8ac295e3aa62293c174409ba83b514ccad4a9ddf
Sambhav <Sambhicool@gmail.com>
2025-12-11 14:26:19 -0500

    python

---
+++ callcalc.py
@@ -0,0 +1,2 @@
+import mycalc as calc
+print(calc.add_numbers(10, 5))
---
+++ mycalc.py
@@ -0,0 +1,11 @@
+def add_numbers(a, b):
+    return a + b
+def subtract_numbers(a, b):
+    return a - b
+def multiply_numbers(a, b):
+    return a * b
+
+print(add_numbers(2, 3))
+print(subtract_numbers(5, 2))
+print(multiply_numbers(4, 3))
+
